d0debf282c174db213b1cf64848c08a05609f05c
Michał Modzelewski <michal.modzelewski@gmail.com>
2025-03-16 20:37:43 +0100

    fix undervoltage clear condition

--- ADV2/BMS/Source/App/BMS_Protection.c
+++ ADV2/BMS/Source/App/BMS_Protection.c
@@ -159,7 +159,7 @@ void BMS_Undervoltage_Protection(void)
 		
 		for(i=0;i<AFE_MAX_CELL_CNT;i++)
 		{
-			if(DVC_1124.Single_Voltage[i] > 3000)
+			if(DVC_1124.Single_Voltage[i] <= 3000)
 			{
 				val2++;
 			}
